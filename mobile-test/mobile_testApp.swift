ad7c533629b156349f68fa4be07ab25103fb8efc
Beto Toro <betotoro0902@gmail.com>
2022-11-19 10:03:20 -0500

    [init] first commit

---
+++ mobile-test/mobile_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  mobile_testApp.swift
+//  mobile-test
+//
+//  Created by Beto Toro on 19/11/22.
+//
+
+import SwiftUI
+
+@main
+struct mobile_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
