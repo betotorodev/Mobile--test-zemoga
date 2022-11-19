7230f22f4345a95c1b61c948c8dce7a329213641
Beto Toro <betotoro0902@gmail.com>
2022-11-19 15:53:38 -0500

    [feat] load post data from the API

--- mobile-test/ContentView.swift
+++ mobile-test/ContentView.swift
@@ -7,19 +7,47 @@
 
 import SwiftUI
 
+struct Response: Codable {
+  var userId: Int
+  var id: Int
+  var title: String
+  var body: String
+}
+
 struct ContentView: View {
+  
+  @State private var results = [Response]()
+  
+  func loadData() async {
+    guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else {
+      print("Invalid URL")
+      return
+    }
+    do {
+      let (data, _) = try await URLSession.shared.data(from: url)
+      if let decodedResponse = try? JSONDecoder().decode([Response].self, from: data) {
+        results = decodedResponse
+      }
+    } catch {
+      print("Invalid data")
+    }
+  }
+  
   var body: some View {
     NavigationView {
       List {
-        ForEach(0..<5, id: \.self) { post in
+        ForEach(results, id: \.id) { post in
           NavigationLink {
-            Text("\(post + 1)")
+            Text("\(post.id)")
           } label: {
-            Text("post \(post + 1)")
+            Text("post \(post.id)")
           }
         }
       }
       .navigationTitle("Posts")
+      .task {
+        await loadData()
+      }
     }
   }
 }
