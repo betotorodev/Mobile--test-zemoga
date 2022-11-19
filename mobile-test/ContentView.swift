a67f09f9e8454f88479246c7b00f92556a145815
Beto Toro <betotoro0902@gmail.com>
2022-11-19 15:13:41 -0500

    [feat] basic layout

--- mobile-test/ContentView.swift
+++ mobile-test/ContentView.swift
@@ -8,19 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+  var body: some View {
+    NavigationView {
+      List {
+        ForEach(0..<5, id: \.self) { post in
+          NavigationLink {
+            Text("\(post + 1)")
+          } label: {
+            Text("post \(post + 1)")
+          }
         }
-        .padding()
+      }
+      .navigationTitle("Posts")
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
