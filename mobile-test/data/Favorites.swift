f4d5dbae856121043a71213fbf72a2b3295987e6
Beto Toro <betotoro0902@gmail.com>
2022-11-22 14:43:44 -0500

    [feat] eliminate some comments

--- mobile-test/data/Favorites.swift
+++ mobile-test/data/Favorites.swift
@@ -8,14 +8,11 @@
 import Foundation
 
 class Favorites: ObservableObject {
-  // the actual posts the user has favorited
   private var posts: Set<String>
   
-  // the key we're using to read/write in UserDefaults
   private let saveKey = "Favorites"
   
   init() {
-    // load our saved data
     if let data = UserDefaults.standard.data(forKey: saveKey) {
       if let decoded = try? JSONDecoder().decode(Set<String>.self, from: data) {
         posts = decoded
@@ -23,23 +20,23 @@ class Favorites: ObservableObject {
       }
     }
     
-    // still here? Use an empty array
+
     posts = []
   }
   
-  // returns true if our set contains this resort
+
   func contains(_ post: Posts) -> Bool {
     posts.contains(String(post.id))
   }
   
-  // adds the resort to our set, updates all views, and saves the change
+
   func add(_ post: Posts) {
     objectWillChange.send()
     posts.insert(String(post.id))
     save()
   }
   
-  // removes the resort from our set, updates all views, and saves the change
+  
   func remove(_ post: Posts) {
     objectWillChange.send()
     posts.remove(String(post.id))
@@ -47,7 +44,6 @@ class Favorites: ObservableObject {
   }
   
   func save() {
-    // write out our data
     if let encoded = try? JSONEncoder().encode(posts) {
       UserDefaults.standard.set(encoded, forKey: saveKey)
     }
